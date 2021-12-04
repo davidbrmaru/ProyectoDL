8e3287d09ab5f217f7791e3767c42e560f6cac71
davidbrmaru <davidbrmaru@gmail.com>
2021-12-04 15:43:23 -0500

    up

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-exec(open("03.Codigo/keras_efficientnet_v2/version.py").read())
+exec(open("keras_efficientnet_v2/version.py").read())
 setup(
     name="keras-efficientnet-v2",
     version=__version__,
     description="(Unofficial) Tensorflow keras efficientnet v2 with pre-trained",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/leondgarse/keras_efficientnet_v2",
+    url="https://github.com/davidbrmaru/keras_efficientnet_v2",
     author="Leondgarse",
     author_email="leondgarse@google.com",
     classifiers=[
